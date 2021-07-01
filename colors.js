ab23800615bf8c98dcc89228f07d40342f676dbd
acutler101 <acutler0451@gmail.com>
2021-07-01 17:03:56 -0400

    initial commit

---
+++ colors.js
@@ -0,0 +1,15 @@
+'use strict';
+const button = document.getElementsByClassName('btn');
+const randomButton = document.getElementById('btn-random');
+
+randomButton.addEventListener('click', function () {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = '#' + randomColor;
+});
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function () {
+    document.body.style.backgroundColor = button[i].textContent;
+  });
+}
+console.log('Hello World!');
