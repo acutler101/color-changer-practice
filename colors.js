03c9f8514bd064d2df7e04d83ce984b70c55abaf
acutler101 <acutler0451@gmail.com>
2021-07-02 12:40:59 -0400

    removed feature

--- colors.js
+++ colors.js
@@ -1,9 +1,7 @@
 'use strict';
 const button = document.getElementsByClassName('btn');
 const randomButton = document.getElementById('btn-random');
-const newFeature = function () {
-  console.log('Welcome to my app!');
-};
+const newFeature = function () {};
 
 randomButton.addEventListener('click', function () {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -15,5 +13,3 @@ for (let i = 0; i < button.length; i++) {
     document.body.style.backgroundColor = button[i].textContent;
   });
 }
-
-newFeature();
